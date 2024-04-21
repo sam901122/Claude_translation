d286b0725374b9e2e4ed3ecba13a7e81ff6a2905
PuSung <sam90.hsu@gmail.com>
2024-04-22 00:18:33 +0800

    comments

--- translator.py
+++ translator.py
@@ -21,8 +21,8 @@ class Translator:
             model (str): the model name, provided by Anthropic
             to_language (str): the target language. Every language is available, it will be sent to the API
             input_file_path (str): the input file path
+            progress_callback (callable): the progress callback function, update the number of paragraphs that have been translated
             stop_event (threading.Event): the stop event
-            progress_callback (function): the progress callback function, update the number of paragraphs that have been translated
         Return:
             None
         """
@@ -32,7 +32,7 @@ class Translator:
         self.progress_callback = progress_callback
         self.stop_event = stop_event
 
-        # read article
+        # Read article and split into paragraphs
         with open(input_file_path, "r", encoding="utf-8") as file:
             self.article = file.read()
         self.paragraphs = self._preprocess(self.article)
@@ -49,10 +49,12 @@ class Translator:
 
     @property
     def translated_cnt(self):
+        """Return the number of translated paragraphs."""
         return len([p for p in self.translated_paragraphs if p != None])
 
     @property
     def paragraph_cnt(self):
+        """Return the number of paragraphs."""
         return len(self.paragraphs)
 
     def _preprocess(self, txt: str):
@@ -74,11 +76,24 @@ class Translator:
         return preprocessed_paragraphs
 
     def _translate_worker(self, par_index_queue):
+        """
+        The work function for the translation thread.
+        This function will read the paragraph index from the queue, translate the paragraph, and update the translated_paragraphs list.
+
+        Args:
+            par_index_queue (Queue): the queue of paragraph indexes
+        Return:
+            None
+        """
+
+        # Keep translating until the queue is empty or the stop event is set
         while self.stop_event.is_set() == False:
             try:
+                # Get the paragraph
                 par_index = par_index_queue.get(block=False)
                 paragraph = self.paragraphs[par_index]
 
+                # Translate the paragraph
                 translated = False
                 while not translated:
                     try:
@@ -88,6 +103,7 @@ class Translator:
                     except:
                         time.sleep(20)  # Wait for 20 seconds and try again
 
+                # Update the progress bar info
                 if self.progress_callback:
                     self.progress_callback(
                         self.translated_cnt, self.paragraph_cnt, translate_preview=translated_paragraph
@@ -97,21 +113,36 @@ class Translator:
                 break
 
     def translate(self, thread_cnt=5):
+        """
+        Translate the whole article paragraph by paragraph.
+        The implementation is multi-threaded, which means it will translate multiple paragraphs at the same time.
+
+        Args:
+            thread_cnt (int): the number of threads for translation
+        Return:
+            str: the translated text
+        """
+
+        # Prepare the queue of paragraph indexes
         par_index_queue = Queue()
         par_index_queue.queue.extend(range(len(self.paragraphs)))
-        threads = []
 
+        # Start the translation threads
+        threads = []
         for _ in range(thread_cnt):
             thread = threading.Thread(target=self._translate_worker, args=(par_index_queue,))
             thread.start()
             threads.append(thread)
 
+        # Wait for all threads to finish
         for thread in threads:
             thread.join()
 
+        # Check if the translation is stopped
         if self.stop_event.is_set():
             self.progress_callback(self.translated_cnt, self.paragraph_cnt, stopped=True)
             return None
 
+        # Combine the translated paragraphs
         translated_txt = "\n\n".join(self.translated_paragraphs)
         return translated_txt
--- ui.py
+++ ui.py
@@ -7,11 +7,14 @@ from translator import Translator
 
 class TranslationUI:
     def __init__(self):
+        """Init the TranslationUI"""
+
         # Window setting
         self.window = tk.Tk()
         self.window.title("Claude translator")
         self.window.geometry("800x600")
 
+        # Model name mapping
         self.model_name_mapping = {
             "Haiku": "claude-3-haiku-20240307",
             "Sonnet": "claude-3-sonnet-20240229",
@@ -34,27 +37,50 @@ class TranslationUI:
         self.create_ui_elmts()
 
     def create_ui_elmts(self):
+        """
+        Create the UI elements, contains:
+            - Choose input file path
+            - Choose model
+            - Choose language
+            - Translation relevant
+        """
+
         def create_input_file_path_elmt():
+            """Create the input file path element"""
             input_file_frame = tk.Frame(self.window)
             input_file_frame.pack(pady=10)
+
+            # Button to select file
             self.select_file_button = tk.Button(input_file_frame, text="Select file", command=self.select_input_file)
             self.select_file_button.pack(side=tk.LEFT, padx=5)
+
+            # Entry to show the file path
             file_entry = tk.Entry(input_file_frame, textvariable=self.input_file_path, width=100)
             file_entry.pack(side=tk.LEFT)
 
         def create_model_elmt():
+            """Create the model element"""
             model_frame = tk.Frame(self.window)
             model_frame.pack(pady=5)
+
+            # Model label
             model_label = tk.Label(model_frame, text="Translation Model:")
             model_label.pack(side=tk.LEFT, padx=5)
+
+            # Model dropdown
             self.model_dropdown = tk.OptionMenu(model_frame, self.model, *(list(self.model_name_mapping.keys())))
             self.model_dropdown.pack(side=tk.LEFT)
 
         def create_language_elmt():
+            """Create the language element"""
             language_frame = tk.Frame(self.window)
             language_frame.pack(pady=5)
+
+            # Language label
             language_label = tk.Label(language_frame, text="Target Language:")
             language_label.pack(side=tk.LEFT, padx=5)
+
+            # Language dropdown
             self.language_choices = [
                 "繁體中文",
                 "English",
@@ -65,23 +91,29 @@ class TranslationUI:
             self.language_dropdown.pack(side=tk.LEFT)
 
         def create_translate_elmt():
+            """Create the translation element"""
             translate_frame = tk.Frame(self.window)
             translate_frame.pack(pady=5)
 
+            # Start translation button
             self.start_btn = tk.Button(translate_frame, text="Start Translation", command=self.translate)
             self.start_btn.pack(side=tk.LEFT)
 
+            # Stop translation button
             self.stop_btn = tk.Button(
                 translate_frame, text="Stop translation", command=self.stop_translate, state="disabled"
             )
             self.stop_btn.pack(side=tk.LEFT)
 
+            # Progress bar
             self.progress_bar = ttk.Progressbar(translate_frame, length=300, mode="determinate")
             self.progress_bar.pack(side=tk.LEFT)
 
+            # Progress info label
             self.progress_info_label = tk.Label(translate_frame, text="")
             self.progress_info_label.pack(side=tk.LEFT)
 
+            # Translate preview
             translate_preview_frame = tk.Frame(self.window)
             translate_preview_frame.pack(pady=10)
 
@@ -94,33 +126,57 @@ class TranslationUI:
         create_translate_elmt()
 
     def stop_translate(self):
+        """Stop the translation process, set the stop event & update the progress info label"""
         self.stop_translation_event.set()
         self.progress_info_label.config(text="Translation stopping...")
 
     def select_input_file(self):
+        """Select the input file path and set the input_file_path variable"""
         file_path = filedialog.askopenfilename(title="Select file", filetypes=[("Text Files", "*.txt")])
         self.input_file_path.set(file_path)
 
     def update_progress(self, translated_cnt, par_cnt, stopped=False, translate_preview=""):
+        """
+        Callback function to update the progress bar and progress info label
+        This function will be called by the Translator class, each time a paragraph is translated
+
+        Args:
+            translated_cnt (int): the number of translated paragraphs
+            par_cnt (int): the total number of paragraphs
+            stopped (bool): whether the translation is stopped
+            translate_preview (str): the preview of the translated paragraph
+        Returns:
+            None
+        """
+
+        # Update the progress bar
         progress = translated_cnt / par_cnt * 100
         self.progress_bar["value"] = progress
 
+        # Update the progress info label
         if self.stop_translation_event.is_set():
             if stopped == False:
                 self.progress_info_label.config(text="Translation stopping...")
             else:
                 self.progress_info_label.config(text="Translation stopped")
         else:
+            # Show the ratio of translated paragraphs and paragraphs preview
             self.progress_info_label.config(text=f"{translated_cnt}/{par_cnt} paragraphs")
             self.translate_preview.config(text=f"Translated Preview: {translate_preview[:20]}...")
 
         self.window.update_idletasks()
 
     def translate(self):
+        """Start the translation process in a new thread"""
+
         def translate_thread():
-            self.toggle_widgets("disabled")
+            """Translate the input file and write the translated text to a new file"""
+
+            # Update button state when translation starts
+            self.update_widgets_state("disabled")
             self.stop_btn.config(state="normal")
 
+            # Start the translation
             self.translator = Translator(
                 self.model_name_mapping[self.model.get()],
                 self.to_language.get(),
@@ -130,24 +186,39 @@ class TranslationUI:
             )
             translated_txt = self.translator.translate()
 
+            # Write the translated text to a new file
             if not self.stop_translation_event.is_set():
                 with open(
                     self.input_file_path.get().replace(".", f"_{self.to_language.get()}."), "w", encoding="utf-8"
                 ) as file:
                     file.write(translated_txt)
 
-            self.toggle_widgets("normal")
+            # Update button state when translation ends
+            self.update_widgets_state("normal")
             self.stop_btn.config(state="disabled")
             self.stop_translation_event.clear()
 
         threading.Thread(target=translate_thread).start()
 
-    def toggle_widgets(self, state):
+    def update_widgets_state(self, state):
+        """
+        Update the state of the widgets, disable or enable them. This function will update the following widgets:
+            - select_file_button
+            - model_dropdown
+            - language_dropdown
+            - start_btn
+
+        Args:
+            state (str): the state to set, "disabled" or "normal"
+        Returns:
+            None
+        """
+
         self.select_file_button.config(state=state)
         self.model_dropdown.config(state=state)
         self.language_dropdown.config(state=state)
         self.start_btn.config(state=state)
 
     def run(self):
+        """Run the UI"""
         self.window.mainloop()
-        pass
